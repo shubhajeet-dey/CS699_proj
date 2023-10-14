aa3c2278f47eef296e55165a1fa14c800816c05b
23M0819_23M0816 <23M0819_23M0816@iitb.ac.in>
2023-10-14 21:36:55 +0530

    Added setup.sh and requirements.txt to create venv

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Installing virtualenv package to create isolated Python 3 virtual environment
+python3 -m pip install virtualenv
+
+# Creating virtual environment named venv_proj
+python3 -m virtualenv venv_proj
+
+# Activating the environment
+source ./venv_proj/bin/activate
+
+# Installing necessary Python packages
+python3 -m pip install -r ./requirements.txt
